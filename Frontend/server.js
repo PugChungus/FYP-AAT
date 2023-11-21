d333a7859efdc20f90ae2e24b1edd56605a9e875
RYANLEEZX <92773637+RYANLEEZX@users.noreply.github.com>
2023-11-21 16:01:00 +0800

    v1

---
+++ Frontend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Set up a route to render your HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/pages/', 'login.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
