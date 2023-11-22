7a521ce16e9469aaacb6debee299cbf1931773fb
RYANLEEZX <92773637+RYANLEEZX@users.noreply.github.com>
2023-11-22 16:49:21 +0800

    OSAKA

--- Frontend/public/js/callnavbar.js
+++ Frontend/public/js/callnavbar.js
@@ -1,8 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
+    // Show the loader
+    document.querySelector('.loader').style.display = 'block';
+
     // Fetch and include the navigation bar
     fetch("component/navbar.html")
         .then(response => response.text())
         .then(html => {
+            // Insert the navigation bar content
             document.getElementById("navbar-container").innerHTML = html;
+
+            // Hide the loader after one second
+            setTimeout(function() {
+                document.querySelector('.loader').style.display = 'none';
+            }, 500); // 1000 milliseconds = 1 second
         });
 });
