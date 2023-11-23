5b7a94555da98d4757884396870bc6eba31895ff
RYANLEEZX <92773637+RYANLEEZX@users.noreply.github.com>
2023-11-23 16:51:23 +0800

    NavBar

--- Frontend/public/js/callnavbar.js
+++ Frontend/public/js/callnavbar.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+function includeNavbar() {
     // Show the loader
     document.querySelector('.loader').style.display = 'block';
 
@@ -7,11 +7,36 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.text())
         .then(html => {
             // Insert the navigation bar content
-            document.getElementById("navbar-container").innerHTML = html;
+            document.getElementById("nav-bar").innerHTML = html;
 
             // Hide the loader after one second
             setTimeout(function() {
                 document.querySelector('.loader').style.display = 'none';
-            }, 500); // 1000 milliseconds = 1 second
+                
+                // Set the active link based on the current page after loader is hidden
+                setActiveLink();
+            }, 500);
         });
+}
+
+function setActiveLink() {
+    // Get the current page URL
+    var currentUrl = window.location.pathname;
+
+    // Remove the 'active' class from all navigation links
+    var navLinks = document.querySelectorAll('.nav_link');
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+
+    // Add the 'active' class to the link corresponding to the current page
+    var activeLink = document.querySelector(`.nav_link[href="${currentUrl}"]`);
+    if (activeLink) {
+        activeLink.classList.add('active');
+    }
+}
+
+// Call the function when the DOM content is loaded
+document.addEventListener("DOMContentLoaded", function() {
+    includeNavbar();
 });
--- Frontend/public/js/navbar.js
+++ Frontend/public/js/navbar.js
@@ -1,6 +1,49 @@
-document.body.classList.add('loading');
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM Content Loaded");
 
-// After content is loaded, remove the loading class
-window.addEventListener('load', function () {
-    document.body.classList.remove('loading');
+  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    console.log("Inside showNavbar function");
+
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId),
+      bodypd = document.getElementById(bodyId),
+      headerpd = document.getElementById(headerId);
+
+    // Validate that all variables exist
+    if (toggle && nav && bodypd && headerpd) {
+      console.log("All elements found");
+
+      toggle.addEventListener('click', () => {
+        console.log("Toggle clicked");
+
+        // show navbar
+        nav.classList.toggle('show');
+        // change icon
+        toggle.classList.toggle('bx-x');
+        // add padding to body
+        bodypd.classList.toggle('body-pd');
+        // add padding to header
+        headerpd.classList.toggle('body-pd');
+      });
+    } else {
+      console.log("Some elements not found");
+    }
+  };
+
+  showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header');
+
+  /*===== LINK ACTIVE =====*/
+  const linkColor = document.querySelectorAll('.nav_link');
+
+  function colorLink() {
+    if (linkColor) {
+      linkColor.forEach(l => l.classList.remove('active'));
+      this.classList.add('active');
+      console.log("Link clicked - colorLink function");
+    }
+  }
+
+  linkColor.forEach(l => l.addEventListener('click', colorLink));
+
+  console.log("End of script - DOM fully loaded and ready");
 });
