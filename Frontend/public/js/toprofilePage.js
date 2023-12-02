6d44e4c71980cda11271eb9c8e9e8407d0373933
RYANLEEZX <92773637+RYANLEEZX@users.noreply.github.com>
2023-12-02 19:08:48 +0800

    Main Profile BS

---
+++ Frontend/public/js/toprofilePage.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var headerImg = document.querySelector('.header_img');
+
+    headerImg.addEventListener('click', function(){
+        window.location.href = 'profile.html'
+    })
+})
