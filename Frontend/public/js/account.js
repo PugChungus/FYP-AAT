eb08c2ae2aafc2004a358f88bc0f9538afde1e8a
Jas2213 <jasonteo1303@gmail.com>
2024-01-08 15:21:17 +0800

    json web token testing
    bro this school shit ain it

--- Frontend/public/js/account.js
+++ Frontend/public/js/account.js
@@ -246,6 +246,8 @@ async function login(event) {
       console.log("hello")
       const data = await response.json();
       if (data.result) {
+        
+        document.cookie = `jwtToken=${data.JWTtoken}; SameSite=Strict; Secure`;
         const count = data.result[0]['count(*)'];
       
         if (count === 1) {
@@ -262,7 +264,8 @@ async function login(event) {
           sessionStorage.setItem('username', username);
           sessionStorage.setItem('email', email_addr);
           sessionStorage.setItem('profile_picture', pfp);
-          
+          document.cookie = `jwtToken=${token}; SameSite=Strict; Secure`;
+
           window.location.href = 'http://localhost:3000/pages/home.html';
         } else {
           alert("Login Failed");
