b5b9b079a3597cac5919e2091a736243eebb4d0b
Vorzhe <trevortanwc04@outlook.com>
2024-01-23 10:46:26 +0800

    w

--- Frontend/server.js
+++ Frontend/server.js
@@ -35,21 +35,18 @@ let keys = {
 };
 
 const rotateKeys = () => {
-    // Generate new keys
     keys.secretJwtKey = crypto.randomBytes(32).toString('hex');
     keys.secret_key = crypto.randomBytes(32).toString('hex');
-    keys.secret_iv = crypto.randomBytes(16).toString('hex');
-
-    console.log('Keys rotated:', keys.secretJwtKey);
 };
 
 // Schedule the key rotation at 00:00 every day
 cron.schedule('0 0 * * *', rotateKeys);
 
-// Example: Log the keys every minute for testing
-cron.schedule('* * * * *', () => {
-    console.log('Current Keys:', keys.secretJwtKey, keys.secret_key, keys.secret_iv);
-});
+// // Example: Log the keys every minute for testing
+// cron.schedule('* * * * *', () => {
+//     console.log('Signature Key', keys.secretJwtKey);
+//     console.log('AES Key', keys.secret_key);
+// });
 
 function checkTokenValidity(authorizationHeader) {
     if (!authorizationHeader) {
@@ -164,7 +161,7 @@ function authorizeRoles() {
             const jwtToken = req.cookies?.jwtToken;
 
             if (!jwtToken) {
-                return res.status(401).json({ message: 'Please return to the login page to renew your token.' });
+                res.status(401).render('accessdenied');
             }
 
             const decodedToken = jwt.verify(jwtToken, keys.secretJwtKey);
@@ -177,19 +174,19 @@ function authorizeRoles() {
                     if (role === 'user') {
                         next();
                     } else {
-                        res.status(403).json({ message: 'Insufficient permissions' });
+                        res.status(401).render('accessdenied');
                     }
                 })
                 .catch(error => {
                     console.error('Error during decryption:', error);
-                    res.status(500).json({ message: 'Internal Server Error' });
+                    res.status(401).render('accessdenied');
                 });
         } catch (error) {
             if (error.name === 'TokenExpiredError') {
-                return res.status(401).json({ message: 'Token has expired. Please log in again.' });
+                res.status(401).render('accessdenied');
             }
 
-            res.status(401).json({ message: 'Unauthorized' });
+            res.status(401).render('accessdenied');
         }
     };
 }
