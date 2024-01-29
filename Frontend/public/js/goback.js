788178f6cf7ab47107f819944e420abf844bd34b
RYANLEEZX <ryanleezhengxian@gmail.com>
2024-01-29 11:22:38 +0800

    profile go back button fixed?!?!? (i think)

--- Frontend/public/js/goback.js
+++ Frontend/public/js/goback.js
@@ -3,16 +3,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (goBackButton) {
         goBackButton.addEventListener("click", function (event) {
-            event.preventDefault();
+            event.preventDefault(); // Prevent the default action of the anchor element
 
-            // Get the previous page's URL using document.referrer
-            var previousPageUrl = document.referrer;
+            // Get the previous page's URL from sessionStorage
+            var previousPageUrl = sessionStorage.getItem("currentPage");
 
-            // Log the intended URL before redirection with a timeout of 2000 milliseconds (2 seconds)
+            // Log the intended URL before redirection
             console.log('Intended URL:', previousPageUrl);
 
             // Redirect to the previous page
             window.location.href = previousPageUrl;
+            sessionStorage.setItem('currentPage', "")
         });
     }
 });
