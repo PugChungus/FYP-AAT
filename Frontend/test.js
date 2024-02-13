2c4a48283b32bd7127da18c82ee9d4934ef97ee1
Vorzhe <trevortanwc04@outlook.com>
2024-02-14 01:58:45 +0800

    w

--- Frontend/test.js
+++ Frontend/test.js
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-var data = "513e2bb4e26ad8a2f1bcf51bb53a0b207d5e33567bc75aeec31d209c573c47975f63b390e20eaee4b06ec7c51d1ae12c8194b623099872b225e7cbbb1f13b486c82bb6aa6e664ee6fd726b316873db8aa4f9aaa48bfea65819d81a109d511c82215269af";
+var data = "192.168.3.3";
 const encryption_key = "846cfa5d2e8dd94532d81a21eacc99d7f7a9bf5317fe2f7a52f3a646ac85a97d"
 
-var encrypted = CryptoJS.AES.encrypt(process.env.ADMIN_PASSWORD, encryption_key);
+var encrypted = CryptoJS.AES.encrypt(data, encryption_key);
 console.log(encrypted.toString());
 
 // console.log(process.env.MYSQL_HOST)
